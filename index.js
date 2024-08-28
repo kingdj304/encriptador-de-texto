fd150ead6662fac042138ac0dfb1cf35df80250c
kingdj304 <danny.11.p@hotmail.com>
2024-08-27 20:28:05 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+function encriptar() {
+    // Obtener el texto ingresado y los elementos donde se mostrarán los mensajes
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+
+    // Encriptar el texto
+    let textoCifrado = texto
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat");
+
+    // Verificar si el texto no está vacío
+    if (texto.length !== 0) {
+        // Mostrar el texto encriptado
+        tituloMensaje.textContent = "Texto encriptado con éxito";
+        parrafo.textContent = textoCifrado;
+    } else {
+        // Mostrar mensaje de alerta si el texto está vacío
+        alert("Debes ingresar algún texto");
+        tituloMensaje.textContent = "";
+        parrafo.textContent = "";
+    }
+}
+
+function desencriptar() {
+    // Obtener el texto ingresado y los elementos donde se mostrarán los mensajes
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+
+    // Desencriptar el texto
+    let textoDesencriptado = texto
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
+
+    // Verificar si el texto no está vacío
+    if (texto.length !== 0) {
+        // Mostrar el texto desencriptado
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = textoDesencriptado;
+    } else {
+        // Mostrar mensaje de alerta si el texto está vacío
+        alert("Debes ingresar algún texto");
+        tituloMensaje.textContent = "";
+        parrafo.textContent = "";
+    }
+}
+
